3be36548abb4af04e7554e273d62111d05bc1a66
Oscar Cortazar-Luebbert <oscar_cortazar@hotmail.com>
2016-10-18 13:05:19 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Output 1 - 255");
+            for(int i = 1; i < 256; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Output 1 - 100");
+            for(int i = 1; i < 101; i++)
+            {
+                if((i % 3 == 0 || i % 5 == 0) && !(i % 3 == 0 && i % 5 == 0))
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("Output 'Fizz', 'Buzz' or 'FizzBuzz'");
+            for(int i = 1; i < 101; i++)
+            {
+                if(i % 3 == 0 && i % 5 == 0)
+                {
+                    Console.WriteLine(i + " FizzBuzz" );
+                }
+                else if(i % 3 == 0)
+                {
+                    Console.WriteLine(i + " Fizz" );
+                }
+                else if(i % 5 == 0)
+                {
+                    Console.WriteLine(i + " Buzz" );
+                }
+            }
+        }
+    }
+}
